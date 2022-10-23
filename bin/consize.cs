fb0198450fe4327b2afd5664d43b8a6644cbd0dc
xiyori <foma@shipilov.ru>
2022-10-23 15:58:26 +0300

    Win console size C# script

---
+++ bin/consize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBuffer {
+    class consize {
+        static void Main(string[] args) {
+            if (args.Length >= 2) {
+                Console.WindowWidth = int.Parse(args[0]);
+                Console.WindowHeight = int.Parse(args[1]);
+            }
+            if (args.Length == 4) {
+                Console.BufferWidth = int.Parse(args[2]);
+                Console.BufferHeight = int.Parse(args[3]);
+            }
+        }
+    }
+}
