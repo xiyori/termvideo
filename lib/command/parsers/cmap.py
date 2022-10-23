a360012504fc21d77f7f0ccd3cbc3e30fa4be181
xiyori <foma@shipilov.ru>
2022-10-23 15:54:31 +0300

    More flexible cmap parser

--- lib/command/parsers/cmap.py
+++ lib/command/parsers/cmap.py
@@ -1,15 +1,23 @@
 from .base import BaseParser
+from ..option import Option
 from lib.colors import cmap
 
 
 class CmapParser(BaseParser):
-    def __init__(self):
-        super(CmapParser, self).__init__(
+    def __init__(self, shortened: bool = False):
+        super().__init__(
             name="cmap",
-            mapping={"common": cmap.common,
-                     "bad_apple": cmap.bad_apple,
-                     "lagtrain": cmap.lagtrain},
+            mapping=None,
             default=cmap.common,
             unwrapped=False,
-            shortened=False
+            shortened=shortened
         )
+
+    def __call__(self, option: Option) -> int:
+        if (option.name == self._name or
+                (self._shortened and option.name == self._name[0])):
+            try:
+                return getattr(cmap, option.value)
+            except BaseException:
+                pass
+        raise IndexError("no suitable conversion")
