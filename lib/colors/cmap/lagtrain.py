e823885a2a76b62d9f8a00d16bdc37cd8d732546
xiyori <foma@shipilov.ru>
2022-10-23 16:01:38 +0300

    Better lagtrain cmap

--- lib/colors/cmap/lagtrain.py
+++ lib/colors/cmap/lagtrain.py
@@ -4,9 +4,9 @@ from ..color_palette import Color
 
 
 def lagtrain(img: np.ndarray, back: int = 178,
-             eps: tuple = (6, 7)) -> np.ndarray:
+             eps: tuple = (11, 7)) -> np.ndarray:
     grayscale = img.mean(axis=-1)
-    out = np.full_like(grayscale, Color.LIGHTBLACK_EX.value, dtype=np.uint8)
+    out = np.full_like(grayscale, Color.WHITE.value, dtype=np.uint8)
     out[back + eps[1] < grayscale] = Color.LIGHTWHITE_EX.value
     out[grayscale < back - eps[0]] = Color.BLACK.value
     return out
