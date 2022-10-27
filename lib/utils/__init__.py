83afc6ede1fbd36fd65828d898276ff6af59fefc
xiyori <foma@shipilov.ru>
2022-10-27 12:53:05 +0300

    Remove old manuals

--- lib/utils/__init__.py
+++ lib/utils/__init__.py
@@ -1,6 +1,5 @@
 from .color_string import ColorString
 from .cursor import show_cursor, hide_cursor, move_cursor
-from .get_manual import get_manual
 from .get_optimal_size import get_optimal_size
 from .graceful_killer import GracefulKiller
 from .perf_counter_ms import perf_counter_ms
